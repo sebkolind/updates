1952910e9baada14998655539ac00e5f9a0367fc
sebkolind <sks1993@gmail.com>
2024-12-02 20:14:17 +0100

    fix(queue): handle invalid json

--- src/commands/queue/utils.ts
+++ src/commands/queue/utils.ts
@@ -4,9 +4,28 @@ import { FILE_PATH } from "./constants.ts";
  * Read the queue from the file.
  */
 async function readQueue(): Promise<string[]> {
-  const content = await Deno.readTextFile(FILE_PATH);
+  try {
+    await Deno.lstat(FILE_PATH);
 
-  return content ? JSON.parse(content) : [];
+    const content = await Deno.readTextFile(FILE_PATH);
+    const tasks = parseJSON(content);
+
+    return tasks ?? [];
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      return [];
+    }
+
+    throw error;
+  }
+}
+
+function parseJSON(content: string): string[] {
+  try {
+    return JSON.parse(content);
+  } catch (_) {
+    throw new SyntaxError("Invalid JSON content.");
+  }
 }
 
 export { readQueue };
