c6310d220ca24cb27e11597bd4d671243c5a11c8
sebkolind <sks1993@gmail.com>
2024-12-09 20:58:45 +0100

    Add GitHub provider

--- src/github/fetcher.ts
+++ src/github/fetcher.ts
@@ -1,35 +1,29 @@
-import { load } from "../config.ts";
 import type { Task } from "../types/task.ts";
+import { load } from "../config.ts";
+import { Octokit } from "octokit";
 
 const config = await load();
-
-const apiUrl = "https://api.github.com";
 const token = config.providers.github.api.key;
 
-async function fetchGitHubIssue(issueId: string): Promise<any> {
-  const response = await fetch(
-    `${apiUrl}/repos/${config.providers.github.repo}/issues/${issueId}`,
-    {
-      headers: {
-        "Authorization": `token ${token}`,
-        "Accept": "application/vnd.github.v3+json",
-      },
-    },
-  );
+const octokit = new Octokit({ auth: token });
 
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error(`Failed to fetch issue: ${response.status}`);
-  }
+async function fetchGitHubIssue(issueId: number) {
+  const [owner, repo] = config.providers.github.repo.split("/");
+  const { data } = await octokit.rest.issues.get({
+    owner,
+    repo,
+    issue_number: issueId,
+  });
+
+  return data;
 }
 
 async function githubFetcher(taskIds: string[]): Promise<Task[]> {
   return await Promise.all(taskIds.map(async (id) => {
-    const issue = await fetchGitHubIssue(id);
+    const issue = await fetchGitHubIssue(parseInt(id));
 
     return {
-      id: issue.id,
+      id: issue.id.toString(),
       identifier: id,
       title: issue.title,
       url: issue.html_url,
